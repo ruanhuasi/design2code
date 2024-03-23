2bc8741380f3508d22fd9abcaad9b9635668c989
ruanhausi <280128121@qq.com>
2024-03-23 22:30:13 +0800

    build: 新增代理路径

--- src/lib/openai.ts
+++ src/lib/openai.ts
@@ -3,7 +3,7 @@ import { HttpsProxyAgent } from 'https-proxy-agent'
 
 export function openaiClient(apiKey: string): OpenAI {
   return new OpenAI({
-    httpAgent: new HttpsProxyAgent('https://api.nextapi.fun'),
+    // httpAgent: new HttpsProxyAgent('https://api.nextapi.fun'),
     apiKey: apiKey,
   });
 }
